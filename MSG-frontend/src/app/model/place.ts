c704a54f4d720194eac52e3785d2b7d720838340
Simone Colaci <sim.colaci@gmail.com>
2018-07-05 08:54:13 +0200

    Model classes

---
+++ MSG-frontend/src/app/model/place.ts
@@ -0,0 +1,9 @@
+export class Place {
+  id: number;
+  name: string;
+  location: string;
+  address  : string;
+  postalNo: string;
+  email: string;
+  telephone: string;
+};
---
+++ MSG-frontend/src/app/model/visit.ts
@@ -0,0 +1,14 @@
+import { Place } from './place';
+
+export class Visit {
+  id: number;
+  badgeNo: string;
+  hostName: string;
+  guestName: string;
+  guestDocumentType: string;
+  guestDocumentNo: string;
+  entranceDate: string;
+  leavingDate: string;
+  entranceTime: string;
+  leavingTime: string;
+};
