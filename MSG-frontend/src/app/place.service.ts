5c9bb0b8f307c617d37f4e1d86962eec2f08c430
Simone Colaci <sim.colaci@gmail.com>
2018-07-06 11:28:52 +0200

    crud services to communicate with backend

---
+++ MSG-frontend/src/app/place.service.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Place } from './model/place';
+import { BE_URL } from './data/be_url';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlaceService {
+  private placeUrl = BE_URL+'/api/place';
+  private placesUrl = BE_URL+'/api/places';
+  currentPlace: Place;
+  
+  constructor(
+    private http: HttpClient
+  ) { }
+  setCurrentPlace(place: Place):void {
+    this.currentPlace = place;
+  }
+  getCurrentPlace():Place {
+    return this.currentPlace;
+  }
+  getPlaces(): Observable<Place[]> {
+    return this.http.get<Place[]>(this.placesUrl)
+      .pipe(
+        tap(places => this.log(`fetched places`)),
+        catchError(this.handleError<Place[]>('getPlaces', []))
+      );
+  }
+
+  getPlace(id: number): Observable<Place> {
+    const url = `${this.placeUrl}/${id}`;
+    return this.http.get<Place>(url)
+      .pipe(
+        tap(_ => this.log(`fetched place id=${id}`)),
+        catchError(this.handleError<Place>(`getPlace id=${id}`))
+      );
+  }
+
+  updatePlace (place: Place): Observable<any> {
+    return this.http.put(this.placeUrl, place, httpOptions)
+      .pipe(
+        tap(_ => this.log(`updated place id=$=${place.id}`)),
+        catchError(this.handleError<any>('updatePlace'))
+      );
+  }
+
+  addPlace (place: Place): Observable<Place> {
+    return this.http.post<Place>(this.placeUrl, place, httpOptions)
+      .pipe(
+        tap((place: Place) => this.log(`added place w/ id=${place.id}`)),
+        catchError(this.handleError<Place>(`addPlace`))
+      );
+  }
+
+  deletePlace (place: Place | number): Observable<Place> {
+    const id = typeof place === 'number' ? place : place.id;
+    const url = `${this.placeUrl}/${id}`;
+
+    return this.http.delete<Place>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted place id=${id}`)),
+      catchError(this.handleError<Place>('deletePlace'))
+    );
+  }
+
+  private log(message:string): void {
+    console.log('PlaceService:' + message);
+  }
+
+  handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
---
+++ MSG-frontend/src/app/visit.service.ts
@@ -0,0 +1,108 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Visit } from './model/visit';
+import { BE_URL } from './data/be_url';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VisitService {
+  private visitUrl = BE_URL+'/api/visit';
+  private visitsUrl = BE_URL+'/api/visits';
+  private visitsByPlaceAndNameUrl = BE_URL+'/api/visit_by_place_and_name';
+  private visitsByPlaceAndDateUrl = BE_URL+'/api/visit_by_place_and_date';
+
+  constructor(
+    private http: HttpClient,
+    private datePipe: DatePipe
+  ) { }
+
+  getVisits(): Observable<Visit[]> {
+    return this.http.get<Visit[]>(this.visitsUrl)
+      .pipe(
+        tap(visits => this.log(`fetched visits`)),
+        catchError(this.handleError<Visit[]>('getVisits', []))
+      );
+  }
+
+  getVisit(id: number): Observable<Visit> {
+    const url = `${this.visitUrl}/${id}`;
+    return this.http.get<Visit>(url)
+      .pipe(
+        tap(_ => this.log(`fetched visit id=${id}`)),
+        catchError(this.handleError<Visit>(`getVisit id=${id}`))
+      );
+  }
+
+  updateVisit (visit: Visit): Observable<any> {
+    return this.http.put(this.visitUrl, visit, httpOptions)
+      .pipe(
+        tap(_ => this.log(`updated visit id=$=${visit.id}`)),
+        catchError(this.handleError<any>('updateVisit'))
+      );
+  }
+
+  addVisit (visit: Visit): Observable<Visit> {
+    return this.http.post<Visit>(this.visitUrl, visit, httpOptions)
+      .pipe(
+        tap((visit: Visit) => this.log(`added visit w/ id=${visit.id}`)),
+        catchError(this.handleError<Visit>(`addVisit`))
+      );
+  }
+
+  deleteVisit (visit: Visit | number): Observable<Visit> {
+    const id = typeof visit === 'number' ? visit : visit.id;
+    const url = `${this.visitUrl}/${id}`;
+
+    return this.http.delete<Visit>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted visit id=${id}`)),
+      catchError(this.handleError<Visit>('deleteVisit'))
+    );
+  }
+
+  getVisitsByDate(placeId:number, date: Date): Observable<Visit[]> {
+    const dateStr = this.datePipe.transform(date, 'yyyy-MM-dd');
+    return this.http.get<Visit[]>(`${this.visitsByPlaceAndDateUrl}?place_id=${placeId}&date=${dateStr}`)
+      .pipe(
+        tap(_ => this.log(`found visits matching "${date}" with place id "${placeId}"`)),
+        catchError(this.handleError<Visit[]>('searchVisits', []))
+      );
+  }
+
+  getVisitsByName(placeId:number, name: string): Observable<Visit[]> {
+    if (!name.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Visit[]>(`${this.visitsByPlaceAndNameUrl}?place_id=${placeId}&name='${name}'`)
+      .pipe(
+        tap(_ => this.log(`found visits matching "${name}" with place id "${placeId}"`)),
+        catchError(this.handleError<Visit[]>('searchVisits', []))
+      );
+  }
+
+  private log(message:string): void {
+    console.log('VisitService:' + message);
+  }
+
+  handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
