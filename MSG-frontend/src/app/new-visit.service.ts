397b0e69a177e4e8a01c40b455bb3bf5b79c54a7
Simone Colaci <sim.colaci@gmail.com>
2018-07-06 11:28:11 +0200

    added new-visit service
    this service communicate the visit information to craete visit from an existing one

---
+++ MSG-frontend/src/app/new-visit.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, EventEmitter, Output } from '@angular/core';
+
+import { Visit } from './model/visit';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewVisitService {
+  visit: Visit;
+  showForm:boolean;
+
+  constructor() { }
+
+  @Output() changeForm: EventEmitter<boolean> = new EventEmitter();
+  @Output() changeVisit: EventEmitter<Visit> = new EventEmitter();
+
+  set(visit:Visit) {
+    this.visit = visit;
+    this.changeVisit.emit(this.visit);
+  }
+  show(){
+    this.showForm = true;
+    this.changeForm.emit(this.showForm);
+  }
+  hide(){
+    this.showForm = false;
+    this.changeForm.emit(this.showForm);
+  }
+}
