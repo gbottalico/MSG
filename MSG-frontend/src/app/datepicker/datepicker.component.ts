bda02c5508dda98b722bffa15bddfdb028063be8
Simone Colaci <sim.colaci@gmail.com>
2018-07-06 11:26:35 +0200

    added datepicker component
    this component handle the datepicker on the left

---
+++ MSG-frontend/src/app/datepicker/datepicker.component.ts
@@ -0,0 +1,35 @@
+import { Component, Injectable, OnInit } from '@angular/core';
+import { NgbDatepickerI18n, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+
+import { I18n, CustomDatepickerI18n } from '../data/datepicker_IT';
+import { DatepickerService } from '../datepicker.service';
+
+const now = new Date();
+
+@Component({
+  selector: 'app-datepicker',
+  templateUrl: './datepicker.component.html',
+  styleUrls: ['./datepicker.component.css'],
+  providers: [I18n, {provide: NgbDatepickerI18n, useClass: CustomDatepickerI18n}] // define custom NgbDatepickerI18n provider
+})
+
+export class DatepickerComponent implements OnInit{
+  model: NgbDateStruct;
+  date: Date;
+
+  constructor(private datepickerService : DatepickerService) { }
+
+  ngOnInit(){
+    this.selectToday();
+  }
+
+  selectToday() {
+    this.model = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
+    this.bindDate();
+  }
+
+  bindDate(){
+    this.date = new Date(this.model.year, this.model.month-1, this.model.day);
+    this.datepickerService.bind(this.date);
+  }
+}
