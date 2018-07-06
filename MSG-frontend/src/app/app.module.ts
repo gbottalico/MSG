936af9065156c6fb181f7c0c317b6ba45f490fd8
Simone Colaci <sim.colaci@gmail.com>
2018-07-06 11:23:09 +0200

    configuration

--- MSG-frontend/src/app/app.module.ts
+++ MSG-frontend/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgModule, Input, LOCALE_ID } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewVisitComponent } from './new-visit/new-visit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DatepickerComponent,
+    DashboardComponent,
+    NewVisitComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
