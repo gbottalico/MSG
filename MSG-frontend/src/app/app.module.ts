8e51a2fe1c5acff1f01e2754b66eb939c7845b48
Simone Colaci <sim.colaci@gmail.com>
2018-07-09 11:50:44 +0200

    config

--- MSG-frontend/src/app/app.module.ts
+++ MSG-frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { FormsModule } from '@angular/forms';
 import { NgModule, Input, LOCALE_ID } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -11,6 +10,9 @@ import { HeaderComponent } from './header/header.component';
 import { DatepickerComponent } from './datepicker/datepicker.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NewVisitComponent } from './new-visit/new-visit.component';
+import { CookieService } from 'ngx-cookie-service';
+import { EditVisitComponent } from './edit-visit/edit-visit.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { NewVisitComponent } from './new-visit/new-visit.component';
     HeaderComponent,
     DatepickerComponent,
     DashboardComponent,
-    NewVisitComponent
+    NewVisitComponent,
+    EditVisitComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +31,7 @@ import { NewVisitComponent } from './new-visit/new-visit.component';
     HttpClientModule,
     NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ MSG-frontend/src/app/data/document_types.ts
@@ -0,0 +1,1 @@
+export const DOCUMENT_TYPES: string[] = [ "Patente", "Passaporto", "Carta D'Identità" ];
