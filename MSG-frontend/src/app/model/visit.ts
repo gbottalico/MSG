19c32a514c05b2eae790abe339a995ccc98784a3
Simone Colaci <sim.colaci@gmail.com>
2018-07-09 11:52:59 +0200

    fixed

--- MSG-frontend/src/app/model/visit.ts
+++ MSG-frontend/src/app/model/visit.ts
@@ -1,16 +1,16 @@
-import { Time } from '@angular/common'
 import { Place } from './place';
 
 export class Visit {
-  id: number;
-  badgeNo: string;
-  hostName: string;
-  guestName: string;
-  guestDocumentType: string;
-  guestDocumentNo: string;
-  entranceDate: string;
-  leavingDate: string;
-  entranceTime: string;
-  leavingTime: string;
-  socialReason: string;
+  public id: number;
+  public badgeNo: string;
+  public hostName: string;
+  public guestName: string;
+  public guestDocumentType: string;
+  public guestDocumentNo: string;
+  public entranceDate: string;
+  public leavingDate: string;
+  public entranceTime: string;
+  public leavingTime: string;
+  public socialReason: string;
+  public place: Place;
 };
